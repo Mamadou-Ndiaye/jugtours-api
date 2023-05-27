010325e543bed2ee1bebcbe1892b308c3c5f8e65
M ND <mamadou.ndiaye@free-partenaires.sn>
2023-05-27 14:06:31 +0000

    Fichier main + les tests

--- src/main/java/sn/ucad/jugtours/JugtoursApplication.java
+++ src/main/java/sn/ucad/jugtours/JugtoursApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JugtoursApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JugtoursApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JugtoursApplication.class, args);
+    }
 
 }
--- src/test/java/sn/ucad/jugtours/JugtoursApplicationTests.java
+++ src/test/java/sn/ucad/jugtours/JugtoursApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JugtoursApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
